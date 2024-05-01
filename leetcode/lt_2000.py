6a3fa6d4a04cd42c75b5e2cc255dbedc216d085f
Emele-Onu Favour <emeleonufavour15@gmail.com>
2024-05-01 09:22:54 +0100

    Solved Leetcode 2000

--- leetcode/lt_2000.py
+++ leetcode/lt_2000.py
@@ -6,17 +6,33 @@ def reverse_string(string :str):
     else:
         return string[-1] + reverse_string(string[:(string_length-1)]) 
     
-# class Solution:
-#     def reversePrefix(self, word: str, ch: str) -> str:
-#         prefix = ""
-#         for index,value in enumerate(word):
-#             if value == ch:
-#                 prefix = word[:index]
-                
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        if len(word) == 0:
+            return ""
+        if len(word) == 1:
+            return word
+        prefix = ""
+        new_word = ""
+        for index,value in enumerate(word):
+            if value == ch:
+                prefix = word[:index+1]
+                new_word = word[index+1:]
+                break
+        if prefix == "" and new_word == "":
+            return  word
+        else:
+            return reverse_string(prefix) + new_word
             
   
 word = "abced"
-last = word[-1]
-new = word[:2+1]
-print(f"last: {last}, new: {new}")
+# last = word[-1]
+# new = word[:2+1]
+# new_2 = word[2+1:]
+# print(f"last: {last}, new: {new}, new 2: {new_2}")
+
+if __name__ == "__main__":
+    sol = Solution()
+    result = sol.reversePrefix(word, "z")
+    print(result)
 
